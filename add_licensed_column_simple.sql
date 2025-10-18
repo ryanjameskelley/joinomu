6ed072253370a58571444091be8fb96695a34535
ryanjameskelley <ryanj.kelley@icloud.com>
2025-10-17 18:59:41 -0700

    edits

--- add_licensed_column_simple.sql
+++
@@ -1,5 +0,0 @@
--- Simple fix to add the licensed column
-ALTER TABLE providers ADD COLUMN licensed TEXT[] DEFAULT '{}';
-
--- Test that it worked
-SELECT column_name FROM information_schema.columns WHERE table_name = 'providers' AND column_name = 'licensed';
